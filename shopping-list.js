e3f1248f1db21e7fd5eada8c2f023818410cf6f9
mashhour1 <42009405+mashhour1@users.noreply.github.com>
2023-03-29 13:16:36 -0400

    Create shopping-list.js

---
+++ shopping-list.js
@@ -0,0 +1,24 @@
+// Create an empty array to store the shopping list items
+var shoppingList = [];
+
+// Function to add a new item to the shopping list
+function addItem() {
+	var itemInput = document.getElementById("item-input");
+	var newItem = itemInput.value;
+	if (newItem !== "") {
+		shoppingList.push(newItem);
+		itemInput.value = "";
+		updateList();
+	}
+}
+
+// Function to update the shopping list displayed on the page
+function updateList() {
+	var listElement = document.getElementById("shopping-list");
+	listElement.innerHTML = "";
+	for (var i = 0; i < shoppingList.length; i++) {
+		var itemElement = document.createElement("li");
+		itemElement.textContent = shoppingList[i];
+		listElement.appendChild(itemElement);
+	}
+}
