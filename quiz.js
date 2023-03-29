73aba02e6e447abd811265a79ba389deb9c0b098
mashhour1 <42009405+mashhour1@users.noreply.github.com>
2023-03-29 12:47:05 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -5,7 +5,10 @@ function submitQuiz() {
 	// Get the user's answers
 	var q1Answer = quizForm.elements["q1"].value;
 	var q2Answer = quizForm.elements["q2"].value;
-
+	var q3Answer = quizForm.elements["q3"].value;
+	var q4Answer = quizForm.elements["q4"].value;
+	var q5Answer = quizForm.elements["q5"].value;
+	
 	// Calculate the score
 	var score = 0;
 	if (q1Answer === "a") {
@@ -14,8 +17,18 @@ function submitQuiz() {
 	if (q2Answer === "a") {
 		score++;
 	}
+	if (q3Answer === "b") {
+		score++;
+	}
+	if (q4Answer === "a") {
+		score++;
+	}
+	if (q5Answer === "a") {
+		score++;
+	}
+
 
 	// Display the score
-	var scoreText = "You scored " + score + " out of 2";
+	var scoreText = "You scored " + score + " out of 5";
 	document.getElementById("score").innerHTML = scoreText;
 }
